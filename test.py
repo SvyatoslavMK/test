d04ba37d59a45698cffe75b1716b8e3499a00d26
MaSvKir <masv@gmail.com>
2023-10-31 19:30:58 +0100

    hii

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print("hello")
+class Student():
+  def __init__(self, name, height):
+    self.name = name
+    self.height= 160
+
+    
+
+
+st1 = Student("bob",160)
+st2 = Student("kate",150)
+
+
+
+
+
