bcd0dc9b625e9aae6e5fc59ef8954a6f27ef1d2d
MaSvKir <masv@gmail.com>
2023-11-03 19:28:26 +0100

    new file

--- test.py
+++ test.py
@@ -1,16 +1,1 @@
-class Student():
-  happy = 20
-  def __init__(self, name, height=160):
-    self.name = name
-    self.height= height
-    self.happy +=10
-  def show(self):
-    print(self.name,self.height,self.happy)
-
-st1 = Student("bob")
-st2 = Student("kate",150)
-
-st1.show()
-st2.show()
-
-
+hello to the teacher
